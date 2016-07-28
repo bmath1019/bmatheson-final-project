25a72c5a38f888e75d7d78ba08a832cd20567f3e
bmath1019 <bmath1019@gmail.com>
2016-07-28 00:22:40 -0400

    Path SVG Plotted - First Attempt

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 var app;
 
-// Declaring our constants
-var START_YEAR = 1950;
-var END_YEAR = 2015;
-var MAX_RADIUS = 50;
-var TRANSITION_DURATION = 750;
-
 // d3.queue() enables us to load multiple data files. Following the example below, we make
 // additional .defer() calls with additional data files, and they are returned as results[1],
 // results[2], etc., once they have all finished downloading.
@@ -21,11 +15,18 @@ app = {
   components: [],
 
   options: {
-    year: START_YEAR
+    type: 'reports'
   },
 
   initialize: function (data) {
     app.data = data;
+
+    var parseDate = d3.timeParse("%m/%d/%Y"); //https://bl.ocks.org/d3noob/0e276dc70bb9184727ee47d6dd06e915
+
+    data.forEach(function(d) {
+        d.date = parseDate(d.date)
+        d.reports = +d.reports;
+    });
 
     // Here we create each of the components on our page, storing them in an array
     app.components = [
@@ -33,29 +34,12 @@ app = {
     ];
 
     // Add event listeners and the like here
+    d3.select('#type').on('change', function () {
+      options.type = d3.event.target.value;
+      charts.forEach(function (chart) {chart.update();});
+    });
 
-    // app.resize() will be called anytime the page size is changed
-    d3.select('window').on('resize', app.resize);
-
-    // For demo purposes, let's tick the year every 750ms
-    function incrementYear() {
-      app.options.year += 1;
-      if (app.options.year > END_YEAR) {
-        app.options.year = START_YEAR;
-      }
-
-      app.update();
-    }
-
-    d3.interval(incrementYear, TRANSITION_DURATION);
-  },
-
-  resize: function () {
-    app.components.forEach(function (c) { if (c.resize) { c.resize(); }});
-  },
-
-  update: function () {
-    app.components.forEach(function (c) { if (c.update) { c.update(); }});
+    app.update();
   }
 }
 
@@ -68,7 +52,7 @@ function Chart(selector) {
     top: 15, right: 15, bottom: 40, left: 45
   };
 
-  chart.width = 600 - margin.left - margin.right;
+  chart.width = 1000 - margin.left - margin.right;
   chart.height = 400 - margin.top - margin.bottom;
 
   chart.svg = d3.select(selector)
@@ -80,21 +64,18 @@ function Chart(selector) {
 
   // SCALES
 
-  chart.x = d3.scaleLinear()
-    .domain([0, d3.max(app.data, function (d) { return d.total_fertility; })])
+  chart.x = d3.scaleTime()
+    .domain([d3.min(app.data, function (d) { return d.date; }), d3.max(app.data, function (d) { return d.date; })])
     .range([0, chart.width])
     .nice();
 
+  var maxF3 = d3.max(app.data, function (d) { return d.f3; })
+  var maxF3X = d3.max(app.data, function (d) { return d.f3x; })
+
   chart.y = d3.scaleLinear()
-    .domain([0, d3.max(app.data, function (d) { return d.life_expectancy; })])
+    .domain([0, d3.max([maxF3,maxF3X])])
     .range([chart.height, 0])
     .nice();
-
-  chart.r = d3.scaleSqrt()
-    .domain([0, d3.max(app.data, function (d) { return d.population; })])
-    .range([0, MAX_RADIUS]);
-
-  chart.color = d3.scaleOrdinal(d3.schemeCategory10);
 
   // AXES
 
@@ -114,7 +95,7 @@ function Chart(selector) {
     .style('text-anchor', 'end')
     .style('fill', '#000')
     .style('font-weight', 'bold')
-    .text('Fertility (births per woman)');
+    .text('Date');
 
   chart.svg.append('g')
     .attr('class', 'y axis')
@@ -127,20 +108,7 @@ function Chart(selector) {
     .style('text-anchor', 'end')
     .style('fill', '#000')
     .style('font-weight', 'bold')
-    .text('Life expectancy (years)');
-
-  // YEAR LABEL
-
-  chart.svg.append('text')
-    .attr('class', 'year')
-    .attr('x', chart.width / 2)
-    .attr('y', chart.height / 2)
-    .attr('dy', '.35em')
-    .style('text-anchor', 'middle')
-    .style('font-size', '230px')
-    .style('font-weight', 'bold')
-    .style('opacity', 0.2)
-    .text(app.options.year);
+    .text('Log Number of Reports');
 
   chart.update();
 }
@@ -151,36 +119,44 @@ Chart.prototype = {
 
     // TRANSFORM DATA
 
-    var txData = app.data.filter(function (d) { return d.year === app.options.year; });
+    var txData = app.data.slice();
+
+      // var txData = app.data.filter(function (d) { return d.reports === app.options.reports; });
 
     // UPDATE CHART ELEMENTS
 
-    var t = d3.transition().duration(TRANSITION_DURATION);
+    var lineF3 = d3.line()
+      .x(function (d) { return chart.x(d.date)})
+      .y(function (d) { return chart.y(d.f3)})
 
-    var yearText = d3.selectAll('.year')
-      .transition().delay(TRANSITION_DURATION / 2)
-      .text(app.options.year);
+    var lineF3X = d3.line()
+      .x(function (d) { return chart.x(d.date)})
+      .y(function (d) { return chart.y(d.f3x)})
 
-    var countries = chart.svg.selectAll('.country')
-      .data(txData, function (d) {return d.country;});
+    var lineF5 = d3.line()
+      .x(function (d) { return chart.x(d.date)})
+      .y(function (d) { return chart.y(d.f5)})
 
-    countries.enter().append('circle')
-      .attr('class', 'country')
-      .style('fill', function (d) { return chart.color(d.continent); })
-      .style('opacity', 0.75)
-      .attr('r', 0)
-      .attr('cx', chart.width / 2)
-      .attr('cy', chart.height / 2)
-      .merge(countries)
-      .sort(function (a, b) { return b.population - a.population; })
-      .transition(t)
-      .attr('r', function (d) { return chart.r(d.population); })
-      .attr('cx', function (d) { return chart.x(d.total_fertility); })
-      .attr('cy', function (d) { return chart.y(d.life_expectancy); });
+    chart.svg.append("path")
+        .datum(txData)
+        .attr("class","line f3")
+        .attr("d",lineF3)
+        .style("opacity",.5)
+        .style("stroke-width",3);    
 
-    countries.exit()
-      .transition(t)
-      .attr('r', 0)
-      .remove();
+    chart.svg.append("path")
+        .datum(txData)
+        .attr("class","line f3x")
+        .attr("d",lineF3X)
+        .style("opacity",.5)
+        .style("stroke-width",3);
+
+    chart.svg.append("path")
+        .datum(txData)
+        .attr("class","line f5")
+        .attr("d",lineF5)
+        .style("opacity",.5)
+        .style("stroke-width",3);        
+
   }
 }
